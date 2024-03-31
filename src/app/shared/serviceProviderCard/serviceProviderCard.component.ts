43487a28be6035051c2ebde26688318174b7132c
unknown <abadianparsa51@gmail.com>
2024-03-31 09:36:20 +0330

    first marge

--- src/app/shared/serviceProviderCard/serviceProviderCard.component.ts
+++ src/app/shared/serviceProviderCard/serviceProviderCard.component.ts
@@ -11,6 +11,26 @@ export class serviceProviderCardComponent {
   @Input() cardData: ServiceProviderCardModel[] = [];
   @Output() cardClicked = new EventEmitter<string>();
 
+  items: { label: string, component: any }[] = [
+    { label: 'نمونه کارها', component: 1 },
+    { label: 'مشخصات', component: 2 },
+    { label: 'Tab 3', component: 3 },
+    { label: 'Tab 4', component: 4 },
+    { label: 'Tab 5', component: 5 },
+    { label: 'Tab 6', component: 6 },
+  ];
+  selectedTab: string | undefined;
+  selectedIndex: number = 0;
+  selectTab(index: number) {
+    this.selectedIndex = index;
+  }
+
+  onTabChange(event: MatTabChangeEvent) {
+    this.selectedTab = this.items[event.index].label;
+  }
+
+
+
   order() {
     alert("شکیبا باشید !")
   }
